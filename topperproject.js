06167cf080c420868f0da5f08461934dd8267c80
elroi773 <s2457@e-mirim.hs.kr>
2024-09-10 00:02:13 +0900

    에러 해결

--- topperproject.js
+++ topperproject.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const goook = document.getElementById('character-goook');
     const choyoung = document.getElementById('character-choyoung');
     const descriptionBox = document.getElementById('description-goook');
-    
+
     // 국연수 이미지 클릭 시 오디오 재생
     goook.addEventListener('click', function() {
         var audio = document.getElementById('audio');
+        audio.load(); // 오디오 파일 명시적으로 로드
         if (audio.paused) {
             audio.play().catch(function(error) {
                 console.error("오디오 재생 중 오류:", error);
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 버튼 클릭 시 오디오 재생
     document.querySelector('.button').addEventListener('click', function() {
         var audio = document.getElementById('audio-player');
+        audio.load(); // 오디오 파일 명시적으로 로드
         if (audio.paused) {
             audio.play().catch(function(error) {
                 console.error("오디오 재생 중 오류:", error);
@@ -26,5 +28,4 @@ document.addEventListener('DOMContentLoaded', function() {
             audio.pause();
         }
     });
-
 });
