72dfde94a32b94e5b9e1cab33522d45b108dcc45
elroi773 <s2457@e-mirim.hs.kr>
2024-10-21 18:52:47 +0900

    ost music bar js 수정

--- topperimgsong.js
+++ topperimgsong.js
@@ -39,7 +39,7 @@
      // 설명 보이기/숨기기
      description2.style.opacity = description2.style.opacity === "0" || description2.style.opacity === "" ? "1" : "0";
  });
- // 세 번째 이미지 클릭 이벤트
+ // 두 번째 이미지 클릭 이벤트
  const img3 = document.getElementById('playMusic3');
  const audio3 = document.getElementById('music3');
  const description3 = document.getElementById('description3');
@@ -57,7 +57,7 @@
      flexContainer3.classList.toggle('move-down');
 
      // 설명 보이기/숨기기
-     description3.style.opacity = description3.style.opacity === "0" || description2.style.opacity === "" ? "1" : "0";
+     description3.style.opacity = description3.style.opacity === "0" || description3.style.opacity === "" ? "1" : "0";
  });
  // 네 번째 이미지 클릭 이벤트
  const img4 = document.getElementById('playMusic4');
@@ -77,25 +77,26 @@
      flexContainer4.classList.toggle('move-down');
 
      // 설명 보이기/숨기기
-     description4.style.opacity = description4.style.opacity === "0" || description2.style.opacity === "" ? "1" : "0";
+     description4.style.opacity = description4.style.opacity === "0" || description4.style.opacity === "" ? "1" : "0";
  });
+ 
  // 다섯 번째 이미지 클릭 이벤트
- const img5 = document.getElementById('playMusic5');
- const audio5 = document.getElementById('music5');
- const description5 = document.getElementById('description5');
- const flexContainer5 = document.getElementById('flexContainer5');
+    const img5 = document.getElementById('playMusic5');
+    const audio5 = document.getElementById('music5');
+    const description5 = document.getElementById('description5');
+    const flexContainer5 = document.getElementById('flexContainer5');
 
- img5.addEventListener('click', function() {
-     // 음악 재생/일시정지
-     if (audio5.paused) {
-         audio5.play();
-     } else {
-         audio5.pause();
-     }
+    img5.addEventListener('click', function() { // img5로 수정
+        // 음악 재생/일시정지
+        if (audio5.paused) {
+            audio5.play();
+        } else {
+            audio5.pause();
+        }
 
-     // 사진과 설명이 같이 밑으로 이동하는 애니메이션 적용
-     flexContainer5.classList.toggle('move-down');
+        // 사진과 설명이 같이 밑으로 이동하는 애니메이션 적용
+        flexContainer5.classList.toggle('move-down');
 
-     // 설명 보이기/숨기기
-     description5.style.opacity = description2.style.opacity === "0" || description2.style.opacity === "" ? "1" : "0";
- });
+        // 설명 보이기/숨기기
+        description5.style.opacity = description5.style.opacity === "0" || description5.style.opacity === "" ? "1" : "0";
+    });
